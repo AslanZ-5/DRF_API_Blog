e25e64c26467b6d210ed4d0b3159bea4a491cd37
Aslan <asl.zurabov@gmail.com>
2021-10-14 20:51:54 +0300

    dd ; ;

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import (UserCreationForm,
+                                       UserChangeForm,
+                                       PasswordChangeForm
+                                       )
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileform
 from django.contrib.auth.views import PasswordChangeView
@@ -13,17 +16,19 @@ class EditProfilePageView(generic.UpdateView):
     model = Profile
     success_url = reverse_lazy('home')
     template_name = 'registration/edit_profile_page.html'
-    fields = ['image','biograph','fb_url','tw_url','inst_url']
+    fields = ['image', 'biograph', 'fb_url', 'tw_url', 'inst_url']
+
 
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/profile.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ShowProfilePageView,self).get_context_data(*args, **kwargs)
-        page_user = get_object_or_404(Profile,id=self.kwargs['pk'])
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context['page_user'] = page_user
         return context
+
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
